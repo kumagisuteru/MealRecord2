3dc6ae0ad1caba3618e2cc57b531b0151219d914
kumagisuteru <p2od58hx@s.okayama-u.ac.jp>
2019-07-19 03:03:21 +0900

    Merge branch 'master' of https://github.com/kumagisuteru/MealRecord
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/websarva/wings/android/mealrecord/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/mealrecord/MainActivity.java
@@ -62,19 +62,19 @@ public class MainActivity extends AppCompatActivity {
                         Mscore = seekM.getProgress();
 
                         if(Mscore >= 0 && Mscore < 20){
-                            faceScaleImageView.setImageResource(R.drawable.painface1);
+                            faceScaleImageView.setImageResource(R.drawable.face1);
 
                         }else if(Mscore >= 20 && Mscore < 40){
-                            faceScaleImageView.setImageResource(R.drawable.painface2);
+                            faceScaleImageView.setImageResource(R.drawable.face2);
 
                         }else if(Mscore >= 40 && Mscore < 60){
-                            faceScaleImageView.setImageResource(R.drawable.painface3);
+                            faceScaleImageView.setImageResource(R.drawable.face3);
 
                         }else if(Mscore >= 60 && Mscore < 80){
-                            faceScaleImageView.setImageResource(R.drawable.painface4);
+                            faceScaleImageView.setImageResource(R.drawable.face4);
 
                         }else if(Mscore >= 80 && Mscore <= 100){
-                            faceScaleImageView.setImageResource(R.drawable.painface5);
+                            faceScaleImageView.setImageResource(R.drawable.face5);
 
                         }
 
